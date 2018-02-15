871c3b2b68e3fdfe1aaba42bf3e9326152884082
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-15 11:48:23 -0500

    update projects and bio

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //// Router ////
 const pages = [
 	'main',
+	'mono',
 	'flask',
 	'nodejs',
 	'dotnet',
@@ -51,6 +52,33 @@ fetch("dotnet.json").then((response) => {
 			</div>`;
 
 			document.querySelector(".dotnet-list").innerHTML += card;
+		})
+	})
+})
+
+fetch("mono.json").then((response) => {
+	response.json().then(data => {
+		data.projects.map(project => {
+
+			let github = "";
+
+			if(project.github) github = `
+			<a href="${project.github}" target="_blank">
+		        <button class="btn">Github link</button>
+		    </a>`;
+
+			let card = `
+			<div class="card">
+			    <div class="card-title">${project.name}</div>
+			    <p><strong>${project.technologies}</strong></p>
+			    <p>${project.description}</p>
+			    <a href="${project.link}" target="_blank">
+			        <button class="btn">${project.linkType}</button>
+			    </a>
+			    ${github}
+			</div>`;
+
+			document.querySelector(".mono-list").innerHTML += card;
 		})
 	})
 })
