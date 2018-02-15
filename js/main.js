4bad45afa217e95a7c32cd3a118a02a65513ec41
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-15 12:50:11 -0500

    fix typo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //// Router ////
 const pages = [
 	'main',
+	'todo',
 	'mono',
 	'flask',
 	'nodejs',
@@ -160,6 +161,33 @@ fetch("firebase.json").then((response) => {
 			</div>`;
 
 			document.querySelector(".firebase-list").innerHTML += card;
+		})
+	})
+})
+
+fetch("todo.json").then((response) => {
+	response.json().then(data => {
+		data.projects.map(project => {
+
+			let github = "";
+
+			if(project.github) github = `
+			<a href="${project.github}" target="_blank">
+		        <button class="btn">Github link</button>
+		    </a>`;
+
+			let card = `
+			<div class="card">
+			    <div class="card-title">${project.name}</div>
+			    <p><strong>${project.technologies}</strong></p>
+			    <p>${project.description}</p>
+			    <a href="${project.link}" target="_blank">
+			        <button class="btn">${project.linkType}</button>
+			    </a>
+			    ${github}
+			</div>`;
+
+			document.querySelector(".todo-list").innerHTML += card;
 		})
 	})
 })
